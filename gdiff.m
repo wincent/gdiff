cf8804ab0874f87079e3b594fb4536f1698c6829
Wincent Colaiuta <win@wincent.com>
2007-09-21 14:27:11 +0200

    Show usage information when no arguments supplied
    Add stub function for displaying usage information; will need to flesh out
    this information as options are added.
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- gdiff.m
+++ gdiff.m
@@ -1,3 +1,10 @@
+//
+// gdiff.m
+// gdiff
+//
+// Created by Wincent Colaiuta on 21 September 2007.
+// Copyright 2007 Wincent Colaiuta.
+
 // system headers
 #import <Foundation/Foundation.h>
 
@@ -5,26 +12,40 @@
 #import "WODiff.h"
 #import "WODiffMachine.h"
 
-int main (int argc, const char *argv[])
+void usage(const char *executable)
+{
+    fprintf(stderr, "usage: %s PATCHFILE...\n", executable);
+}
+
+int main(int argc, const char *argv[])
 {
     int                 exit_status = EXIT_SUCCESS;
     NSAutoreleasePool   *pool       = [[NSAutoreleasePool alloc] init];
     WODiffMachine       *machine    = [WODiffMachine diffMachine];
 
-    for (int i = 1; i < argc; i++)
+    if (argc == 1)
     {
-        NSError *error;
-        NSData *data = [NSData dataWithContentsOfFile:[NSString stringWithUTF8String:argv[i]] options:0 error:&error];
-        if (!data)
+        usage(argv[0]);
+        exit_status = EXIT_FAILURE;
+    }
+    else
+    {
+        // for now just pre-process the arguments: ultimately will pass these off to the GUI app
+        for (int i = 1; i < argc; i++)
         {
-            fprintf(stderr, "%s\n", [[error localizedDescription] UTF8String]);
-            exit_status = EXIT_FAILURE;
-            continue;
-        }
+            NSError *error;
+            NSData *data = [NSData dataWithContentsOfFile:[NSString stringWithUTF8String:argv[i]] options:0 error:&error];
+            if (!data)
+            {
+                fprintf(stderr, "%s\n", [[error localizedDescription] UTF8String]);
+                exit_status = EXIT_FAILURE;
+                continue;
+            }
 
-        WODiff *diff = [machine parseDiffData:data];
-        if (diff == nil)
-            exit_status = EXIT_FAILURE;
+            WODiff *diff = [machine parseDiffData:data];
+            if (diff == nil)
+                exit_status = EXIT_FAILURE;
+        }
     }
     [pool drain];
     return exit_status;
