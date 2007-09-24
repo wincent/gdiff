2c1f187096e2f3191251b74414f986c9ff974a81
Wincent Colaiuta <win@wincent.com>
2007-09-25 00:29:37 +0200

    Initial import of GUI project
    This is just the freshly-created template for the GUI part of gdiff, with
    default content as provided by Xcode.
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

---
+++ gui/MyDocument.h
@@ -0,0 +1,15 @@
+//
+//  MyDocument.h
+//  gdiff
+//
+//  Created by Wincent Colaiuta on 9/25/07.
+//  Copyright __MyCompanyName__ 2007 . All rights reserved.
+//
+
+
+#import <Cocoa/Cocoa.h>
+
+@interface MyDocument : NSDocument
+{
+}
+@end
