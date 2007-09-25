cadc4d84db52a50f380a4eb672537a4182b5275f
Wincent Colaiuta <win@wincent.com>
2007-09-25 17:03:40 +0200

    Refactoring of path utilities for readability
    For better readability, break out two chunks of the path_for_tool function
    and place them in dedicated functions of their own:
    get_search_path_from_environment and get_search_path_from_sysctl. This
    makes the program flow a little more readable and also enables some neater
    handling of resource cleanup, seeing as now only one of the function
    explicitly mallocs memory.
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- WOPathUtilities.m
+++ WOPathUtilities.m
@@ -7,63 +7,73 @@
 
 #import "WOPathUtilities.h"
 
+// system headers
+#import <sys/sysctl.h>
+
+NSString *get_search_path_from_environment(void)
+{
+    char *path = getenv("PATH");
+    return path ? [NSString stringWithUTF8String:path] : nil;
+}
+
+NSString *get_search_path_from_sysctl(void)
+{
+    char    *path;
+    size_t  path_len;
+    int     mib[] = { CTL_USER, USER_CS_PATH };
+
+    // get size of path string
+    if (sysctl(mib, 2, NULL, &path_len, NULL, 0) != 0)
+    {
+        perror("error: (sysctl)");
+        return nil;
+    }
+
+    // allocate space for string
+    path = malloc(path_len);
+    if (path == NULL)
+    {
+        perror("error: (malloc)");
+        return nil;
+    }
+
+    // actually get string
+    NSString *searchPath = nil;
+    if (sysctl(mib, 2, path, &path_len, NULL, 0) != 0)
+        perror("error: (sysctl)");
+    else // success
+        searchPath = [NSString stringWithUTF8String:path];
+
+    free(path);
+    return searchPath;
+}
+
 NSString *path_for_tool(NSString *tool)
 {
     NSCParameterAssert(tool != nil);
     NSString    *toolPath       = nil;
-    NSString    *searchPaths    = nil;
 
     // get current search path, expect a string like "/usr/bin:/bin:/usr/sbin:/sbin" or similar
-    char        *path           = getenv("PATH");
-    if (path)
-    {
-        searchPaths = [NSString stringWithUTF8String:path];
-        path        = NULL; // avoid double-freeing
-    }
-    else
+    NSString *searchPaths = get_search_path_from_environment();
+    if (!searchPaths)
     {
         fprintf(stderr, "warning: PATH environment variable not set, failing back to sysctl\n");
-        size_t  path_len;
-        int     mib[] = { CTL_USER, USER_CS_PATH };
-
-        // get size of path string
-        if (sysctl(mib, 2, NULL, &path_len, NULL, 0) != 0)
-        {
-            perror("error: (sysctl)");
-            goto bail;
-        }
-
-        // allocate space for string
-        path = malloc(path_len);
-        if (path == NULL)
-        {
-            perror("error: (malloc)");
-            goto bail;
-        }
-
-        // actually get string
-        if (sysctl(mib, 2, path, &path_len, NULL, 0) != 0)
-        {
-            perror("error: (sysctl)");
-            goto bail;
-        }
-        else
-            searchPaths = [NSString stringWithUTF8String:path];
+        searchPaths = get_search_path_from_sysctl();
     }
 
-    NSFileManager *fileManager = [NSFileManager defaultManager];
-    for (NSString *searchPath in [searchPaths componentsSeparatedByString:@":"])
+    if (searchPaths)
     {
-        NSString *checkPath = [searchPath stringByAppendingPathComponent:tool];
-        if ([fileManager isExecutableFileAtPath:checkPath])
+        NSFileManager *fileManager = [NSFileManager defaultManager];
+        for (NSString *searchPath in [searchPaths componentsSeparatedByString:@":"])
         {
-            toolPath = checkPath;
-            goto bail;
+            NSString *checkPath = [searchPath stringByAppendingPathComponent:tool];
+            if ([fileManager isExecutableFileAtPath:checkPath])
+            {
+                toolPath = checkPath;
+                break;
+            }
         }
     }
 
-bail:
-    if (path)
-        free(path);
     return toolPath;
 }
