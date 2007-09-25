bb5fad2aa889813ae1187d347a66efe2ac8f2b51
Wincent Colaiuta <win@wincent.com>
2007-09-25 17:21:58 +0200

    Add application controller
    Add a (currently blank) application controller to the main menu nib. This will
    later be used to manage any application-wide aspects such as preferences (if
    any) and installation of the command-line tool.
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

---
+++ WOAppController.h
@@ -0,0 +1,12 @@
+//
+//  WOAppController.h
+//  gdiff
+//
+//  Created by Wincent Colaiuta on 25 September 2007.
+//  Copyright 2007 Wincent Colaiuta.
+
+@interface WOAppController : NSObject {
+
+}
+
+@end
---
+++ WOAppController.m
@@ -0,0 +1,13 @@
+//
+//  WOAppController.m
+//  gdiff
+//
+//  Created by Wincent Colaiuta on 25 September 2007.
+//  Copyright 2007 Wincent Colaiuta.
+
+// class header
+#import "WOAppController.h"
+
+@implementation WOAppController
+
+@end
