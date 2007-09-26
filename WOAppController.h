194c4f9d59552b2cf5257187de8e6b6f1fe7fda0
Wincent Colaiuta <win@wincent.com>
2007-09-26 13:40:06 +0200

    Hook up user interface for tool installation
    Add a menu item to bring up the installer dialog, set up the bindings and
    connections for initiating installation in response to button clicks, show a
    progress indicator and disable the buttons when an install is in action.
    
    This is only the user interface; the back end has not yet been put in place.
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- WOAppController.h
+++ WOAppController.h
@@ -7,6 +7,20 @@
 
 @interface WOAppController : NSObject {
 
+    BOOL installing;
+
 }
+
+#pragma mark -
+#pragma mark Interface Builder actions
+
+- (IBAction)installForCurrentUser:(id)sender;
+- (IBAction)installForAllUsers:(id)sender;
+- (IBAction)showInstallationHelp:(id)sender;
+
+#pragma mark -
+#pragma mark Properties
+
+@property(readonly,getter=isInstalling) BOOL installing;
 
 @end
--- WOAppController.m
+++ WOAppController.m
@@ -8,6 +8,43 @@
 // class header
 #import "WOAppController.h"
 
+@interface WOAppController ()
+
+#pragma mark -
+#pragma mark Property redeclarations
+
+@property(readwrite) BOOL installing;
+
+@end
+
 @implementation WOAppController
+
+#pragma mark -
+#pragma mark Interface Builder actions
+
+- (IBAction)installForCurrentUser:(id)sender
+{
+    NSAssert(self.installing == NO, @"already installing");
+    self.installing = YES;
+}
+
+- (IBAction)installForAllUsers:(id)sender
+{
+    NSAssert(self.installing == NO, @"already installing");
+    self.installing = YES;
+}
+
+- (IBAction)showInstallationHelp:(id)sender
+{
+
+}
+
+#pragma mark -
+#pragma mark Installation
+
+#pragma mark -
+#pragma mark Properties
+
+@synthesize installing;
 
 @end
