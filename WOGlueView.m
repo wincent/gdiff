63cfac2546b5a6d69fb44d2f6c2f31323c163d77
Wincent Colaiuta <win@wincent.com>
2007-10-01 15:58:37 +0200

    Teach glue view to draw borders
    The WOGlueView class now also draws light gray separator borders (1 pixel
    thick) on the left and right.
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- WOGlueView.m
+++ WOGlueView.m
@@ -8,6 +8,9 @@
 // class header
 #import "WOGlueView.h"
 
+// other headers
+#import "gdiff.h"
+
 @implementation WOGlueView
 
 - (id)initWithFrame:(NSRect)frame {
@@ -18,10 +21,31 @@
     return self;
 }
 
-- (void)drawRect:(NSRect)rect {
-    // Drawing code here.
-    [[NSColor blueColor] set];
-    NSRectFill(rect);
+- (void)drawRect:(NSRect)rect
+{
+    NSRect  bounds          = [self bounds];
+    NSColor *borderColor    = nil;
+
+    // draw left border
+    NSRect border = NSMakeRect(0.0, 0.0, WO_BORDER_WIDTH, bounds.size.height);
+    NSRect draw = NSIntersectionRect(border, rect);
+    if (!NSIsEmptyRect(draw))
+    {
+        borderColor = [NSColor lightGrayColor];
+        [borderColor set];
+        NSRectFill(draw);
+    }
+
+    // draw right border
+    border = NSMakeRect(bounds.size.width - WO_BORDER_WIDTH, 0.0, WO_BORDER_WIDTH, bounds.size.height);
+    draw = NSIntersectionRect(border, rect);
+    if (!NSIsEmptyRect(draw))
+    {
+        // border color most likely is already set
+        if (!borderColor)
+            [[NSColor lightGrayColor] set];
+        NSRectFill(draw);
+    }
 }
 
 @end
