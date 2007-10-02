094f018480fad0bce692433cd0f8e93910368c1d
Wincent Colaiuta <win@wincent.com>
2007-10-02 13:17:58 +0200

    Draw top and bottom borders in file source list
    This is a preliminary attempt at drawing top and bottom borders in the file
    source list. Some visual glitches may be visible near the bottom border if
    the window is rapidly resized; the cause of these will need to be
    investigated. Also, given that the borders are being drawn just inside the
    view it is possible that they could overlap content inside the view; it may
    be necessary to find a way to somehow inset the content or instead perform
    the drawing elsewhere (the split view itself may be a good candidate for
    this as it would localize all top and bottom border drawing to a single
    place in the code).
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- WOSourceOutlineView.m
+++ WOSourceOutlineView.m
@@ -8,6 +8,9 @@
 // class header
 #import "WOSourceOutlineView.h"
 
+// other project headers
+#import "gdiff.h"
+
 @implementation WOSourceOutlineView
 
 #pragma mark NSCoding protocol
@@ -26,6 +29,29 @@
 - (void)drawRect:(NSRect)rect
 {
     [super drawRect:rect];
+
+    // draw top border
+    NSRect  bounds          = [self bounds];
+    NSColor *borderColor    = nil;
+    NSRect  border          = NSMakeRect(0.0, bounds.size.height - WO_BORDER_WIDTH, bounds.size.width, WO_BORDER_WIDTH);
+    NSRect  draw            = NSIntersectionRect(border, rect);
+    if (!NSIsEmptyRect(draw))
+    {
+        borderColor = [NSColor lightGrayColor];
+        [borderColor set];
+        NSRectFill(draw);
+    }
+
+    // draw bottom border
+    border  = NSMakeRect(0.0, 0.0, bounds.size.width, WO_BORDER_WIDTH);
+    draw    = NSIntersectionRect(border, rect);
+    if (!NSIsEmptyRect(draw))
+    {
+        // border color is most likely already set
+        if (!borderColor)
+            [[NSColor lightGrayColor] set];
+        NSRectFill(draw);
+    }
 }
 
 @end
