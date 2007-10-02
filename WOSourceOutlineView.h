f6aa0a36b212aacbded7bedff0836d5c126f4f98
Wincent Colaiuta <win@wincent.com>
2007-10-02 12:57:05 +0200

    Add source list
    Add a source list for showing files included in a patch. This is a
    (currently NSOutlineView subclass); note that in swapping out NSOutlineView
    for the subclass there are serious drawing glitches of the focus ring. A
    fairly typical pattern in AppKit (empty subclass behaves radically different
    to superclass) but hopefully won't be too painful to troubleshoot.
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

---
+++ WOSourceOutlineView.h
@@ -0,0 +1,12 @@
+//
+// WOSourceOutlineView.h
+// gdiff
+//
+// Created by Wincent Colaiuta on 2 October 2007.
+// Copyright 2007 Wincent Colaiuta.
+
+@interface WOSourceOutlineView : NSOutlineView {
+
+}
+
+@end
---
+++ WOSourceOutlineView.m
@@ -0,0 +1,25 @@
+//
+// WOSourceOutlineView.h
+// gdiff
+//
+// Created by Wincent Colaiuta on 2 October 2007.
+// Copyright 2007 Wincent Colaiuta.
+
+// class header
+#import "WOSourceOutlineView.h"
+
+@implementation WOSourceOutlineView
+
+- (id)initWithFrame:(NSRect)frame {
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code here.
+    }
+    return self;
+}
+
+- (void)drawRect:(NSRect)rect {
+    // Drawing code here.
+}
+
+@end
